b098145d0e4ca82524d31580507f3fd9ec76b99e
Wouter <woutervandervelde2000@hotmail.com>
2021-07-31 20:39:01 +0200

    renewed ui for extension settings

--- popup.js
+++ popup.js
@@ -1,13 +1,20 @@
- let changeTime = document.getElementById("time");
+let changeTime = document.getElementById("time");
+let saveButton = document.getElementById("save");
 
- chrome.storage.sync.get("time", ({time}) => {
-     changeTime.value = time;
- })
+chrome.storage.sync.get("time", ({time}) => {
+    changeTime.value = time;
+})
 
+changeTime.addEventListener('change', () => {
+    setTime(changeTime.value)
+})
 
-changeTime.addEventListener('change', async () => {
-    const time = changeTime.value;
+saveButton.addEventListener('click', () => {
+    setTime(changeTime.value)
+})
+
+const setTime = async (time) => {
     chrome.storage.sync.set({time: time}, () => {
         console.log(`updated time to ${time}`)
     });
-})
+}
